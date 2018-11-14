497f8c586cc7af68579e410b479b24612a852500
josephyan0816 <41094548+josephyan0816@users.noreply.github.com>
2018-11-14 17:40:34 +0800

    注意是字符串，不是单个字符

---
+++ 字符串比对.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void mycmp(char *s1,char *s2);
+int main()
+{
+	int i,j;
+	i=0;j=0;
+	
+	char s1[10000];
+	char s2[10000];
+	gets(s1);
+	gets(s2);
+	mycmp(s1,s2);	
+	
+ } 
+ void mycmp(char *s1,char *s2)
+ {
+ 	int idx=0;
+ 	int flag=0;
+ 	int count=0;
+ 	int len1,len2;
+	 len1=strlen(s1);
+ 	len2=strlen(s2);
+ 	for(int i=0;i<len2;i++){
+	 
+ 		for(int j=0;j<len1;j++)
+ 		{
+ 			if(s1[j]!=s2[i+j])
+ 			{
+ 				flag=0;
+ 				break;
+			 }
+			 else flag=1;
+ 		}
+		 	if(flag==1)
+ 			{	count++;	
+ 				printf("%d ",i);
+			 }
+		 
+ }
+ 	if(count==0)printf("-1");
+ 	
+ }
