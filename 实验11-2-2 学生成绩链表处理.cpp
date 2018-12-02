801687e9f59a3dca573408d81b231367d0094365
josephyan0816 <41094548+josephyan0816@users.noreply.github.com>
2018-12-02 10:42:51 +0800

    这题我真是做了一上午，p->next不为空意味着就有下一个节点
    这题我一开始没设置头结点，删除的时候如果有2个节点这种情况我始终处理不好，我感觉这题我真是做不动了，delete部分我是看着别人的代码改的，但是我也不理解为什么要每次删除后从头结点遍历

---
+++ 实验11-2-2 学生成绩链表处理.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stud_node {
+     int    num;
+     char   name[20];
+     int    score;
+     struct stud_node *next;
+};
+
+struct stud_node *createlist();
+struct stud_node *deletelist( struct stud_node *head, int min_score );
+
+int main()
+{
+    int min_score;
+    struct stud_node *p, *head = NULL;
+
+    head = createlist();
+    scanf("%d", &min_score);
+    head = deletelist(head, min_score);
+    for ( p = head; p != NULL; p = p->next )
+        printf("%d %s %d\n", p->num, p->name, p->score);
+
+    return 0;
+}
+struct stud_node *createlist(){
+	int number;
+	struct stud_node *p,*head,*tail;
+	head=(struct stud_node*)malloc(sizeof(struct stud_node));
+	head->next=NULL;
+	p=head;
+	scanf("%d",&number);
+	while(number!=0)
+	{
+		tail=(struct stud_node*)malloc(sizeof(struct stud_node));
+		tail->num=number; 
+		scanf("%s%d",tail->name,&tail->score);
+		
+		p->next=tail;
+		
+		p=tail;
+		tail->next=NULL;
+		scanf("%d",&number); 
+	}
+	return head;
+}
+
+	
+struct stud_node *deletelist( struct stud_node *head, int min_score )
+{
+	struct stud_node *p;
+	p=head;
+	while(p->next!=NULL)
+	{	int flag=0;
+		if(p->next->score<min_score)
+		{
+			struct stud_node *temp;
+			temp=p->next;
+			p->next=temp->next;
+			free(temp);
+			flag=1;
+		}
+		p=p->next;
+		if(flag)
+		p=head;
+	}
+	return head->next;
+}
